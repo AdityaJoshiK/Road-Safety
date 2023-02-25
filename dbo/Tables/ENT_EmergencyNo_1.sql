48511a2ad8bc28a0622e8b314764fbaa578a2d11
Joshi_Aditya <adityajoshi2107@gmail.com>
2023-02-25 15:37:28 +0530

    Aditya-3:337

---
+++ dbo/Tables/ENT_EmergencyNo_1.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[ENT_EmergencyNo] (
+    [EmergencyNumberID] INT      IDENTITY (1, 1) NOT NULL,
+    [CountryID]         INT      NULL,
+    [StateID]           INT      NULL,
+    [CityID]            INT      NULL,
+    [AreaID]            INT      NULL,
+    [EmergencyTypeID]   INT      NULL,
+    [UserID]            INT      NULL,
+    [EmergencyNumber]   INT      NULL,
+    [CreationDate]      DATETIME CONSTRAINT [DF_ENT_EMERGENCYN_CreationDate] DEFAULT (getdate()) NULL,
+    [ModificationDate]  DATETIME CONSTRAINT [DF_ENT_EMERGENCYN_ModificationDate] DEFAULT (getdate()) NULL,
+    CONSTRAINT [PK__ENT_EMER__2248E20FE4FCA818] PRIMARY KEY CLUSTERED ([EmergencyNumberID] ASC)
+);
+
