d57134cdbb6b6716060416fda74e84df74fd0a3c
Joshi_Aditya <adityajoshi2107@gmail.com>
2023-02-25 14:45:40 +0530

    Aditya-ENT_EMERGENCYN,LOC_Area,MST_Hospital,MSt_Roadtype Created

---
+++ dbo/Tables/MST_Hospital.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[MST_Hospital] (
+    [HospitalID]       INT            IDENTITY (1, 1) NOT NULL,
+    [HospitalName]     NVARCHAR (50)  NULL,
+    [HospitalAddress]  NVARCHAR (500) NULL,
+    [CountryID]        INT            NULL,
+    [StateID]          INT            NULL,
+    [CityID]           INT            NULL,
+    [AreaID]           INT            NULL,
+    [HospitalMobileNo] NVARCHAR (10)  NULL,
+    [CreationDate]     DATETIME       DEFAULT (getdate()) NULL,
+    [ModificationDate] DATETIME       DEFAULT (getdate()) NULL,
+    PRIMARY KEY CLUSTERED ([HospitalID] ASC)
+);
+
