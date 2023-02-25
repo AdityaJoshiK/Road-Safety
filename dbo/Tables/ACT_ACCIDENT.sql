5f1dc1a24da5502f8cbe76142f4fca7cd61c62bc
Krutarth Zala <krutarthzala2@gmail.com>
2023-02-25 15:02:10 +0530

    Krutarth Second push

---
+++ dbo/Tables/ACT_ACCIDENT.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[ACT_ACCIDENT] (
+    [AccidentID]          INT            IDENTITY (1, 1) NOT NULL,
+    [AccidentDateTime]    DATETIME       NOT NULL,
+    [AccidentDescription] NVARCHAR (MAX) NOT NULL,
+    [CauseOfAccident]     NVARCHAR (50)  NOT NULL,
+    [NumberOfCasualties]  INT            NOT NULL,
+    [RoadID]              INT            NULL,
+    [HospitalID]          INT            NULL,
+    [VehicalID]           INT            NULL,
+    [CreationDate]        DATETIME       CONSTRAINT [DF_ACT_ACCIDENT_CreationDate] DEFAULT (getdate()) NULL,
+    [ModificationDate]    DATETIME       CONSTRAINT [DF_ACT_ACCIDENT_ModificationDate] DEFAULT (getdate()) NULL,
+    CONSTRAINT [PK_ACT_ACCIDENT] PRIMARY KEY CLUSTERED ([AccidentID] ASC)
+);
+
---
+++ dbo/Tables/WHT_WEATHER.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[WHT_WEATHER] (
+    [WeatherID]            INT            IDENTITY (1, 1) NOT NULL,
+    [WeatherDate]          DATETIME       NOT NULL,
+    [WeatherTemperature]   DECIMAL (5, 2) NOT NULL,
+    [WeatherWindSpeed]     INT            NOT NULL,
+    [WeatherWindDirection] NVARCHAR (50)  NOT NULL,
+    [WeatherHumidity]      INT            NOT NULL,
+    [WeatherPrecipitation] NVARCHAR (MAX) NOT NULL,
+    [CreationDate]         DATETIME       CONSTRAINT [DF_WHT_WEATHER_CreationDate] DEFAULT (getdate()) NULL,
+    [ModificationDate]     DATETIME       CONSTRAINT [DF_WHT_WEATHER_ModificationDate] DEFAULT (getdate()) NULL,
+    CONSTRAINT [PK_WHT_WEATHER] PRIMARY KEY CLUSTERED ([WeatherID] ASC)
+);
+
